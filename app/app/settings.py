3b71448bb670772878ce944b2910d4e56b56ab84
harryakbar <harry.akbar@gudangada.com>
2021-05-04 22:12:29 +0700

    Create user and token

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'core'
+    'rest_framework.authtoken',
+    'core',
+    'user',
 ]
 
 MIDDLEWARE = [
--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from user.serializers import UserSerializers, AuthTokenSerializers
 
-# Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    """Create user in the system"""
+    serializer_class = UserSerializers
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create new auth token for user"""
+    serializer_class = AuthTokenSerializers
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
